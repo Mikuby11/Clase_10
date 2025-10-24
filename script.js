924f0901ed8aa9a9782283789669705ca98840d9
Fernanda Humeres <fda.humeres.lo@gmail.com>
2025-10-24 11:05:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const portfolio = document.querySelector("#portafolio");
 async function datos(raw) {
     try {
         let consulta = await fetch(raw);
-        let trabajos = await consulta.json();
+        let resultado = await consulta.json();
+        let trabajos = resultado.data;
+console.log(trabajos);
         console.log(trabajos);
         trabajos.forEach((trabajo) => {
     portfolio.innerHTML += `
@@ -12,7 +14,7 @@ async function datos(raw) {
                 <img src="${trabajo.imagen}" class="card-img-top" alt="${trabajo.titulo}">
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title">${trabajo.titulo}</h5>
-                    <p class="card-text text-dark">${trabajo.descripcion}</p>
+                    <p class="card-text text-muted text-black">${trabajo.descripcion}</p>
                     <div class="d-flex justify-content-between align-items-center mt-auto">
                         <div class="btn-group">
                             <button type="button" class="btn btn-sm btn-outline-secondary disabled">${trabajo.categoria}</button>
@@ -28,6 +30,4 @@ async function datos(raw) {
     }
 }
 
-
 datos("https://raw.githubusercontent.com/Mikuby11/Clase_10/refs/heads/main/datos.json");
-
